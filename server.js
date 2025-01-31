cf58418894eeafcfd69b56bbbedafe013c0df35d
Alexandru Zaporojan <azaporojan@myseneca.ca>
2025-01-31 12:55:04 -0500

    commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /*********************************************************************************
 
-WEB422 – Assignment 01
+WEB422 – Assignment 02
 I declare that this assignment is my own work in accordance with Seneca 
 Academic Policy. No part of this assignment has been copied manually 
 or electronically from any other source (including 3rd party web sites) or 
